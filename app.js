b178973f66a46efb047ab65ee4e73ce767d44199
islam-Attar <islamattar95@gmail.com>
2022-01-25 14:58:27 +0200

    finished task

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 'use strict';
 
 let allEmployees = []
-function Employee(employeeID, fullName, department, level, salary)
+function Employee(employeeID, fullName, department, level)
 {
 
  this.employeeID = employeeID;
  this.fullName = fullName;
  this.department = department;
  this.level = level;
- this.salary = salary;
- this.image = `./images/${this.fullName}.jpeg`
+ this.salary = 0;
+ this. salaryAfterTax = 0;
+ this.image = `./images/${this.fullName}.jpeg`;
  allEmployees.push(this);
 }
 
@@ -18,47 +19,49 @@ function Employee(employeeID, fullName, department, level, salary)
 
 var department = [ "Administration", "Marketing", "Development", "Finance"];
 var level = ["Junior", "Mid-Senior", "Senior"];
-var salary = 0;
 var salaryAfterTax = 0;
 
-Employee.prototype.getSalary = function(min, max)
+Employee.prototype.getSalary = function()
 {
-    return salary = Math.floor(Math.random() * (max - min)) + min;
+  if(this.level == 'Senior' )
+   return this.salary = Math.floor(Math.random() * (2000 - 1500 + 1) ) + 1500;
+
+  if(this.level == 'Mid-Senior')
+   return this.salary = Math.floor(Math.random() * (1500 - 1000 + 1) ) + 1000;
+
+  if(this.level == 'Junior')
+   return this.salary = Math.floor(Math.random() * (1000 - 500 + 1) ) + 500;
+
 }
 
 Employee.prototype.getTax = function(salary)
 {
-    return salaryAfterTax = salary - (salary* 7.5);
+    return this.salaryAfterTax = salary - (salary * 0.075);
 }
 
 Employee.prototype.render = function()
 {
-  document.write("<h1>EMPLOYEES</h1>")
-  document.write(`<p> ${this.fullName}</p>`)
-  document.write(`<p> ${this.salary}</p>`)
-  document.write(`<img src=${this.image}>`);
+  
+  document.write("<h1>EMPLOYEES</h1>");
 
+  for( let i = 0; i < allEmployees.length; i++)
+  {
+  document.write(`<p> <b>Empolyee name is: ${allEmployees[i].fullName}</b></p>`);
+  document.write(`<p> ${allEmployees[i].fullName} Salary is: ${allEmployees[i].getSalary()}</p>`);
+  document.write(`<p> ${allEmployees[i].fullName} Salary after tax is: ${allEmployees[i].getTax(allEmployees[i].salary)}</p>`);
+  document.write('<br>');
+  }
+  
 }
 
 
-let ghazi = new Employee(1000, "GhaziSamer", "Administration", "Senior", salary);
-let lana = new Employee(1001, "LanaAli", "Finance", "Senior", salary);
-let tamara = new Employee(1002, "TamaraAyoub", "Marketing", "Senior", salary);
+let ghazi = new Employee(1000, "GhaziSamer", "Administration", "Senior");
+let lana = new Employee(1001, "LanaAli", "Finance", "Senior");
+let tamara = new Employee(1002, "TamaraAyoub", "Marketing", "Senior");
+let safi = new Employee(1003, "SafiWalid", "Administration", "Mid-Senior");
+let omar = new Employee(1004, "OmarZaid", "Development", "Senior");
+let rana = new Employee(1005, "RanaSaleh", "Development", "Junior");
+let hadi = new Employee(1006, "HadiAhmad", "Finance", "Mid-Senior");
 
 
-
-
-
-ghazi.getSalary(1500, 2000);
 ghazi.render();
-
-lana.getSalary(1500, 2000);
-lana.render();
-
-tamara.getSalary(1500, 2000);
-tamara.render();
-
-
-
-
-  
